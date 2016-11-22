f7872460cf251f53efe5c7a4bb186b9776217263
xXTHEBEASTXx <toboy000001@gmail.com>
2016-11-22 17:45:42 -0600

    Update meow.java

--- meow.java
+++ meow.java
@@ -2,8 +2,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
+int i = 43;
         System.out.println("meow");
+        System.out.println("cat is " +i+" years old.");
 
   }
 
